074c0463b20856fd26bd7519d7d93e814f668d2e
Bhirahateesvaran Periyasamy <118425824+geek-bhirahatees@users.noreply.github.com>
2023-09-12 14:49:46 +0530

    new react project

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/geek-bhirahatees"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/bhirahateesvaran-periyasamy-47aa14256/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/geek__pragathees__/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Java</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Servlets</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">JDBC</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Bash</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
